793ac8d931cd14cd9143794e13e0748dd2c63cc3
kalb2 <132865166+kalb2@users.noreply.github.com>
2023-08-19 16:00:09 -0600

    First Commit

---
+++ after/client/src/pages/EditPost.jsx
@@ -0,0 +1,63 @@
+import {
+  redirect,
+  useActionData,
+  useLoaderData,
+  useNavigation,
+} from "react-router-dom";
+import { PostForm, postFormValidator } from "../components/PostForm";
+import { getUsers } from "../api/users";
+import { getPost, updatePost } from "../api/posts";
+
+function EditPost() {
+  const { users, post } = useLoaderData();
+  const { state } = useNavigation();
+  const errors = useActionData();
+
+  const isSubmitting = state === "submitting";
+
+  return (
+    <>
+      <h1 className="page-title">Edit Post</h1>
+      <PostForm
+        users={users}
+        defaultValues={post}
+        isSubmitting={isSubmitting}
+        errors={errors}
+      />
+    </>
+  );
+}
+
+async function loader({ request: { signal }, params: { postId } }) {
+  const post = getPost(postId, { signal });
+  const users = getUsers({ signal });
+
+  return { post: await post, users: await users };
+}
+
+async function action({ request, params: { postId } }) {
+  const formData = await request.formData();
+  const title = formData.get("title");
+  const body = formData.get("body");
+  const userId = formData.get("userId");
+
+  const errors = postFormValidator({ title, userId, body });
+
+  if (Object.keys(errors).length > 0) {
+    return errors;
+  }
+
+  const post = await updatePost(
+    postId,
+    { title, body, userId },
+    { signal: request.signal }
+  );
+
+  return redirect(`/posts/${post.id}`);
+}
+
+export const editPostroute = {
+  loader,
+  action,
+  element: <EditPost />,
+};
---
+++ after/client/src/router.jsx
@@ -0,0 +1,66 @@
+import { Navigate, createBrowserRouter, useRouteError } from "react-router-dom";
+import { postListRoute } from "./pages/PostList";
+import { userListRoute } from "./pages/UserList";
+import { todoListRoute } from "./pages/TodoList";
+import { RootLayout } from "./layouts/RootLayout";
+import { postRoute } from "./pages/Post";
+import { userRoute } from "./pages/User";
+import { newPostRoute } from "./pages/NewPost";
+import { editPostroute } from "./pages/EditPost";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Navigate to="/posts" /> },
+          {
+            path: "posts",
+            children: [
+              { index: true, ...postListRoute },
+              {
+                path: ":postId",
+                children: [
+                  { index: true, ...postRoute },
+                  { path: "edit", ...editPostroute },
+                ],
+              },
+              { path: "new", ...newPostRoute },
+            ],
+          },
+          {
+            path: "users",
+            children: [
+              { index: true, ...userListRoute },
+              { path: ":userId", ...userRoute },
+            ],
+          },
+          {
+            path: "todos",
+            ...todoListRoute,
+          },
+          { path: "*", element: <h1>404 - Page Not Found</h1> },
+        ],
+      },
+    ],
+  },
+]);
+
+function ErrorPage() {
+  const error = useRouteError();
+
+  return (
+    <>
+      <h1>Error - Something went wrong</h1>
+      {import.meta.env.MODE !== "production" && (
+        <>
+          <pre>{error.message}</pre>
+          <pre>{error.stack}</pre>
+        </>
+      )}
+    </>
+  );
+}
